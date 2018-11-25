0093691c0430b5cdda27100144f12b5ae08b194b
Daniel Löfquist <daniel@lofquist.org>
2018-11-25 19:56:52 +0100

    Nicer output in web-UI, add build arg to Dockerfile

--- primer-webui/index.js
+++ primer-webui/index.js
@@ -1,9 +1,13 @@
+const apiBaseUrl = 'PRIMER_API_BASE_URL';
+
 function getIsPrime(num) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/primer/api/calculate/is-prime?num=' + num);
+    xhr.open('GET', apiBaseUrl + '/primer/api/calculate/is-prime?num=' + num);
     xhr.onreadystatechange = function(event) {
         console.log(event.target.response);
-        document.getElementById('output-is-prime').innerText = event.target.response;
+        var responseJson = JSON.parse(event.target.response);
+        var isPrime = responseJson.isPrime ? 'yes' : 'no';
+        document.getElementById('output-is-prime').innerText = isPrime;
     }
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
@@ -12,10 +16,11 @@ function getIsPrime(num) {
 function getNthPrime(n) {
     var xhr = new XMLHttpRequest();
     //xhr.open('POST', '');
-    xhr.open('GET', 'http://localhost:8080/primer/api/calculate/nth?n=' + n);
+    xhr.open('GET', apiBaseUrl + '/primer/api/calculate/nth?n=' + n);
     xhr.onreadystatechange = function(event) {
         console.log(event.target.response);
-        document.getElementById('output-nth-prime').innerText = event.target.response;
+        var responseJson = JSON.parse(event.target.response);
+        document.getElementById('output-nth-prime').innerText = responseJson.nth;
     }
     xhr.setRequestHeader('Content-Type', 'application/json');
     //xhr.send(JSON.stringify({n: 213}));
