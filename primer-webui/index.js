be0ed4afebbedddc091af4bf26bbfc87109d57f5
Daniel Löfquist <daniel@lofquist.org>
2018-11-26 17:20:08 +0100

    Fix issue with sed and irritating JS-error

--- primer-webui/index.js
+++ primer-webui/index.js
@@ -3,11 +3,15 @@ const apiBaseUrl = 'PRIMER_API_BASE_URL';
 function getIsPrime(num) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', apiBaseUrl + '/primer/api/calculate/is-prime?num=' + num);
-    xhr.onreadystatechange = function(event) {
-        console.log(event.target.response);
-        var responseJson = JSON.parse(event.target.response);
-        var isPrime = responseJson.isPrime ? 'yes' : 'no';
-        document.getElementById('output-is-prime').innerText = isPrime;
+    xhr.onreadystatechange = function() {
+        console.log(xhr.responseText);
+        var data;
+        var isPrime;
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            isPrime = data.isPrime ? 'yes' : 'no';
+            document.getElementById('output-is-prime').innerText = isPrime;
+        }
     }
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
@@ -17,12 +21,16 @@ function getNthPrime(n) {
     var xhr = new XMLHttpRequest();
     //xhr.open('POST', '');
     xhr.open('GET', apiBaseUrl + '/primer/api/calculate/nth?n=' + n);
-    xhr.onreadystatechange = function(event) {
-        console.log(event.target.response);
-        var responseJson = JSON.parse(event.target.response);
-        document.getElementById('output-nth-prime').innerText = responseJson.nth;
+    xhr.onreadystatechange = function() {
+        console.log(xhr.responseText);
+        var data;
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            document.getElementById('output-nth-prime').innerText = data.nth;
+        }
     }
     xhr.setRequestHeader('Content-Type', 'application/json');
     //xhr.send(JSON.stringify({n: 213}));
     xhr.send();
 }
+
